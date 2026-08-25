6109be9cb588b227e20098689e3ef65a5027d527
ironsilkstrategies <ironsilkstrategies@gmail.com>
2026-08-25 00:26:15 -0500

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export const metadata: Metadata = {
   },
 };
 
-const VERSION = "v2.05";
+const VERSION = "v2.06";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
