228b3da158dd6c1c23f13d4b7408d82f4c6ad70b
ironsilkstrategies <ironsilkstrategies@gmail.com>
2026-08-25 00:49:34 -0500

    Add legacy redirects to Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
+import { redirects as legacyRedirects } from "./lib/org";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return Object.entries(legacyRedirects).map(([source, destination]) => ({
+      source,
+      destination,
+      permanent: true, // 301
+    }));
+  },
 };
 
 export default nextConfig;
