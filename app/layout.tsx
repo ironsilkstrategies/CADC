1141911cab2a07bf62b50b8f658733b207017337
ironsilkstrategies <ironsilkstrategies@gmail.com>
2026-08-31 22:43:48 -0500

    Update version from v2.11 to v2.14

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,7 @@ export const metadata: Metadata = {
   },
 };
 
-const VERSION = "v2.11";
+const VERSION = "v2.14";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
